edc135917cc838c1f781d88eef225742ff9aca85
MK <mk@Narys-MacBook-Pro.local>
2025-04-26 21:54:32 +0700

    crud-nodejs-backend

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route.js")
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// Route
+app.use("/api/products", productRoute )
 
 app.get("/", (req, res) => {
   res.send("Hello worlds from asdasdf");
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({})
-    res.status(200).json(products) 
-  } catch (error) {
-    res.status(500).json({message: error.message})
-  }
-})
-
-app.get("/api/products/:id", async (req, res) => {
-  try {
-    const {id} = req.params
-    const products = await Product.findById(id)
-    res.status(200).json(products)
-  } catch (error) {
-    res.status(500).json({message: error.message})
-  }
-})
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product)
-  } catch (error) {
-    console.log(req.body);
-    res.send(req.body);
-  }
 });
 
 mongoose
--- note.md
+++ note.md
@@ -14,6 +14,21 @@ installation
 1. express - from npm
 2. Mongodb
 
+What I learn about Express
+1. Express.json()
+2. Express.urlencoded()
+3. Express.Router()
+
+NOTE
+
+1. To extend the API from accepting the JSON to accept the FORM input too using the
+* app.use(express.urlencoded({extended: false}))
+
+
+DEVELOPMENT FLOW
+1. Start with tryin it to working with all of the code in one index.js files
+2. Start to Seperate it to the Route and Controller
+
 
 Latest: 
 adding Mongoose Product to Index.js (55:16)
